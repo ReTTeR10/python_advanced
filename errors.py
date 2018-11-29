07975efb478aa79ed965599c19f7e5a0d2dcb90b
Yegor <yegor1993@gmail.com>
2018-11-29 19:50:31 +0300

    ошибки

---
+++ errors.py
@@ -0,0 +1,27 @@
+class UsernameTooLongError(Exception):
+    def __init__(self, username):
+        self.username = username
+
+    def __str__(self):
+        return f'Имя пользователя - {self.username} должно быть менее 26 символов'
+
+
+class ResponseCodeError(Exception):
+    def __init__(self, code):
+        self.code = code
+
+    def __str__(self):
+        return f'Неверный код ответа - {self.code}'
+
+
+class ResponseCodeLenError(Exception):
+    def __str__(self):
+        return f'Неверная длина кода - {self.code}. Длина кода должна быть 3 символа'
+
+
+class MandatoryKeyError(Exception):
+    def __init__(self, key):
+        self.key = key
+
+    def __str__(self):
+        return f'Не хватает обязательного атрибута - {self.key}'
